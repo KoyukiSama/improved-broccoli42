ac715cb56c45e49aa6c96fc0c2afd5a259102dcf
Katarina <kateriiner@cute-huisbaas.localdomain>
2025-04-13 23:03:01 +0200

    add ft_memmove

--- libft.h
+++ libft.h
@@ -9,6 +9,8 @@
 
 // (memory.c)
 void	*ft_memset(void *s, int c, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_bzero(void *s, size_t n);
 
 // (string.c)
--- memory1.c
+++ memory1.c
@@ -54,6 +54,34 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	return (dest);
 }
 
+static void	*ft_memcpy_backwards(void *dest, const void *src, size_t n)
+{
+	const unsigned char	*src_char;
+	const size_t		*src_word;
+	unsigned char		*dst_char;
+	size_t				*dst_word;
+
+	src_char = ((unsigned char *) src) + n - 1;
+	dst_char = ((unsigned char *) dest) + n - 1;
+	while (!is_mem_aligned(dst_char) && n-- > 0)
+		*dst_char-- = *src_char--;
+	if (is_mem_aligned(dst_char) && is_mem_aligned(src_char))
+	{
+		src_word = (size_t *) src_char;
+		dst_word = (size_t *) dst_char;
+		while (n >= word_size())
+		{
+			*dst_word-- = *src_word--;
+			n -= word_size();
+		}
+		src_char = (unsigned char *) src_word;
+		dst_char = (unsigned char *) dst_word;
+	}
+	while (n--)
+		*dst_char-- = *src_char--;
+	return (dest);
+}
+
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
 	const unsigned char	*src_char;
@@ -65,11 +93,26 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 	byte = (unsigned char) c;
 	while (n--)
 	{
-        *dst_char = *src_char;
-        if (*src_char == byte)
-            return (void *) (dst_char + 1);
-        dst_char++;
-        src_char++;
-    }
-    return (NULL);
+		*dst_char = *src_char;
+		if (*src_char == byte)
+			return ((void *) dst_char + 1);
+		dst_char++;
+		src_char++;
+	}
+	return (NULL);
+}
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	const unsigned char	*src_char;
+	const size_t		*src_word;
+	unsigned char		*dst_char;
+	size_t				*dst_word;
+
+	if (dest > src)
+	{
+		return (ft_memcpy_backwards(dest, src, n))
+	}
+	else
+		return (ft_memcpy(dest, src, n));
 }
